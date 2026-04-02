2f36f3603aa67f89180efbf10131b890e3ff4877
MhmdShamii <mhmddshamii@gmail.com>
2026-04-02 21:27:14 +0300

    save

--- src/components/ui/GlassCard.tsx
+++ src/components/ui/GlassCard.tsx
@@ -1,9 +1,11 @@
 
 export default function GlassCard({ children, className }: { children: React.ReactNode; className?: string }) {
     return (
-        <div className={`backdrop-blur-3xl  shadow-xl flex overflow-hidden  "${className || ""}`}
-            style={{ background: "var(--glass-bg)", border: "1px solid var(--glass-border)" }} >
+        <div
+            className={`backdrop-blur-3xl shadow-xl flex ${className || ""}`}
+            style={{ background: "var(--glass-bg)", border: "1px solid var(--glass-border)" }}
+        >
             {children}
-        </ div >
+        </div>
     )
 }
